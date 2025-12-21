cde229ede22a44aa12103a96edd1883e0e426b18
z3nsh0w <131024513+z3nsh0w@users.noreply.github.com>
2025-12-21 14:14:31 +0500

    Preparing 0.2.3 update

--- lib/objects/playlist.dart
+++
@@ -1,9 +0,0 @@
-import 'package:quark/objects/track.dart';
-
-class PlayerPlaylist {
-  final int kind;
-  final String name;
-  final int ownerUid;
-  final List<PlayerTrack> tracks;
-  PlayerPlaylist(this.ownerUid, this.kind, this.name, this.tracks);
-}
--- lib/yandex_music/lib/src/objects/audio_quality.dart
+++
@@ -1,9 +0,0 @@
-enum AudioQuality {
-  lossless('lossless'),
-  normal('nq'),
-  low('lq');
-
-  final String value;
-
-  const AudioQuality(this.value);
-}
